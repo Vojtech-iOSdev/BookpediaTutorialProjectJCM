6e0920a355d2469b062be829f5ee9161f70ad968
Vojtěch Kalivoda <kalivoda167@gmail.com>
2025-04-26 16:35:30 +0200

    [feature] BookSearchBar implementation

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/components/BookSearchBar.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/components/BookSearchBar.kt
@@ -2,36 +2,39 @@ package com.plcoding.bookpedia.book.presentation.book_list.components
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.selection.LocalTextSelectionColors
 import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.key
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.key.Key.Companion.R
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import cmp_bookpedia.composeapp.generated.resources.Res
 import cmp_bookpedia.composeapp.generated.resources.close_hint
 import cmp_bookpedia.composeapp.generated.resources.search_hint
+import cmp_bookpedia.composeapp.generated.resources.search_results
+import com.plcoding.bookpedia.book.presentation.book_list.BookListAction
 import com.plcoding.bookpedia.core.presentation.DarkBlue
 import com.plcoding.bookpedia.core.presentation.DesertWhite
 import com.plcoding.bookpedia.core.presentation.SandYellow
 import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun BookSearchBar(
@@ -85,7 +88,7 @@ fun BookSearchBar(
                             onSearchQueryChange("")
                         }
                     ) {
-                        Icon(
+                        Icon (
                             imageVector = Icons.Default.Close,
                             contentDescription = stringResource(Res.string.close_hint),
                             tint = MaterialTheme.colorScheme.onSurface
@@ -93,12 +96,26 @@ fun BookSearchBar(
                     }
                 }
             },
-            modifier = modifier
+            modifier = Modifier
                 .background(
                     shape = RoundedCornerShape(100),
                     color = DesertWhite
                 )
                 .minimumInteractiveComponentSize()
+        )
+    }
+}
+
+@Preview
+@Composable
+fun BookSearchBarPreview() {
+    MaterialTheme { 
+        BookSearchBar(
+            searchQuery = "BOBBY",
+            onSearchQueryChange = {},
+            onImeSearch = {},
+            modifier = Modifier
+                .fillMaxWidth()
         )
     }
 }
